7a5a182f9bc970f3f2833e346658f543c36c9fcd
Chris Perfect <cperfect@users.noreply.github.com>
2025-04-18 23:46:58 +0000

    initial commit

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+RUST_VERSION="${RUST_VERISON:-stable}"
+set -e
+rustup upgrade ${RUST_VERSION}
+rustup default ${RUST_VERSION}
+rustup component add clippy
+rustup component add rustfmt
+rustup show
